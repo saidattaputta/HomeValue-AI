28da15216baaf306114dc1682ec54dd664a2e5e2
saidattaputta <psd657@gmail.com>
2026-07-09 21:00:34 +0530

    preprocessing file update

--- src/pipelines/preprocessing.py
+++ src/pipelines/preprocessing.py
@@ -1,23 +1,9 @@
-import os
-import joblib
-import pandas as pd
-
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 
 def create_preprocessor(num_cols, cat_cols):
-    """
-    Create a preprocessor for numerical and categorical features.
-
-    Parameters:
-    num_cols (list): List of numerical feature column names.
-    cat_cols (list): List of categorical feature column names.
-
-    Returns:
-    ColumnTransformer: A preprocessor that can be used to transform the data.
-    """
     
     num_pipeline = Pipeline(steps=[
         ('imputer',SimpleImputer(strategy='median')),
